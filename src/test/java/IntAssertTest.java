56abf0227cd0b104da4de8beb36bbb0f46e1971c
igoudt <igoudt@bol.com>
2019-12-26 10:10:47 +0100

    improve unit test coverage

--- src/test/java/IntAssertTest.java
+++ src/test/java/IntAssertTest.java
@@ -1,3 +1,4 @@
+import io.github.ingmargoudt.veritas.Veritas;
 import io.github.ingmargoudt.veritas.assertions.IntAssertion;
 import org.junit.jupiter.api.Test;
 
@@ -5,76 +6,121 @@ import static io.github.ingmargoudt.veritas.Veritas.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class IntAssertTest
-{
+public class IntAssertTest {
 
     @Test
-    public void Integer_isEqualTo(){
+    public void Integer_isEqualTo() {
         assertThat(3).isEqualTo(3);
     }
 
     @Test
-    public void Integer_not_equals(){
+    public void Integer_isNotEqualTo() {
+       assertThrows(AssertionError.class, () -> assertThat(3).isNotEqualTo(3));
+    }
+
+    @Test
+    public void Integer_not_equals() {
         assertThrows(AssertionError.class, () ->
         {
-           assertThat(3).isEqualTo(4);
+            assertThat(3).isEqualTo(4);
         });
     }
 
     @Test
-    public void Integer_customMessage(){
+    public void Integer_customMessage() {
+        Veritas.LOG_PASSED = true;
         IntAssertion success = assertThat(3).onSucces("success");
-        assertEquals("success",success.getCustomPassMessage());
+        assertEquals("success", success.getCustomPassMessage());
+        success.isEqualTo(3);
     }
 
     @Test
-    public void Integer_isEven(){
+    public void Integer_isEven() {
         assertThat(2).isEven();
     }
 
     @Test
-    public void Integer_isOdd(){
+    public void Integer_isNotEven() {
+        assertThrows(AssertionError.class, () -> assertThat(3).isEven());
+    }
+
+    @Test
+    public void Integer_isOdd() {
         assertThat(3).isOdd();
     }
 
     @Test
-    public void isSmallerThan(){
+    public void Integer_isNotOdd() {
+        assertThrows(AssertionError.class, () -> assertThat(4).isOdd());
+    }
+
+    @Test
+    public void isSmallerThan() {
         assertThat(3).isSmallerThan(4);
     }
 
     @Test
-    public void isSmallerEqualTo(){
+    public void isNotSmallerThan(){
+        assertThrows(AssertionError.class, () -> assertThat(3).isSmallerThan(2));
+    }
+
+    @Test
+    public void isSmallerEqualTo() {
         assertThat(3).isSmallerThanOrEqual(3);
         assertThat(3).isSmallerThanOrEqual(4);
     }
 
     @Test
-    public void isGreaterThan(){
+    public void isNotSmallerEqualTo() {
+        assertThrows(AssertionError.class, () -> assertThat(4).isSmallerThanOrEqual(3));
+    }
+
+    @Test
+    public void isGreaterThan() {
         assertThat(4).isGreaterThan(3);
     }
 
     @Test
-    public void isGreaterThanEqualTo(){
+    public void isNotGreaterThan() {
+        assertThrows(AssertionError.class, () -> assertThat(4).isGreaterThan(4));
+    }
+
+    @Test
+    public void isGreaterThanEqualTo() {
         assertThat(4).isGreaterThanOrEqual(4);
         assertThat(5).isGreaterThanOrEqual(4);
     }
 
     @Test
-    public void isBetween(){
-        assertThat(4).isBetween(3,5);
+    public void isNotGreaterOrEqualTo() {
+        assertThrows(AssertionError.class, () -> assertThat(3).isGreaterThanOrEqual(4));
     }
 
     @Test
-    public void isMultipleOf(){
+    public void isBetween() {
+        assertThat(4).isBetween(3, 5);
+    }
+
+    @Test
+    public void isNotBetween() {
+        assertThrows(AssertionError.class, () -> assertThat(3).isBetween(4, 5));
+    }
+
+    @Test
+    public void isMultipleOf() {
         assertThat(15).isMultipleOf(3);
     }
 
     @Test
-    public void testNull(){
-      assertThrows(NullPointerException.class, () ->  assertThat(15).isMultipleOf(null));
+    public void isNotMultipleOf() {
+      assertThrows(AssertionError.class, () -> assertThat(15).isMultipleOf(4));
     }
 
 
+    @Test
+    public void testNull() {
+        assertThrows(NullPointerException.class, () -> assertThat(15).isMultipleOf(null));
+    }
 
 
 }
