085a80c18ff942814cd79a5fff38675623077c48
igoudt <igoudt@bol.com>
2020-01-05 10:51:44 +0100

    rewrite IntegerAssertions to predicate

--- src/main/java/io/github/ingmargoudt/veritas/assertions/IntegerAssertion.java
+++ src/main/java/io/github/ingmargoudt/veritas/assertions/IntegerAssertion.java
@@ -12,91 +12,43 @@ public class IntegerAssertion extends AbstractAssertion<IntegerAssertion, Intege
     }
 
     public IntegerAssertion isEven() {
-        checkIfActualNull();
         return test(n -> n % 2 == 0,"Expected %s to be even, ", actual);
     }
 
     public IntegerAssertion isOdd() {
-        checkIfActualNull();
-        if (actual % 2 != 0) {
-            reportSuccess();
-            return self();
-        }
-        return fail("Expected %s to be odd ", actual);
+        return test (actual -> actual % 2 != 0,"Expected %s to be odd ", actual);
     }
 
     public IntegerAssertion isSmallerThan(Integer target) {
-        checkIfActualNull();
-        if (target == null) {
-            throw new NullPointerException("b can not be null in a < b");
-        }
-        if (actual < target) {
-            reportSuccess();
-            return self();
-        }
-        return fail("Expected %s to be smaller than %s ", actual, target);
+       return  test(actual -> actual < target,"Expected %s to be smaller than %s ", actual, target);
     }
 
     public IntegerAssertion isSmallerThanOrEqual(Integer target) {
-        checkIfActualNull();
-        if (target == null) {
-            throw new NullPointerException("b can not be null in a <= b");
-        }
-        if (actual <= target) {
-            reportSuccess();
-            return self();
-        }
-        return fail("Expected %s to be smaller than or equal to %s ", actual, target);
+        return test(actual -> actual <= target,"Expected %s to be smaller than or equal to %s ", actual, target);
     }
 
     public IntegerAssertion isGreaterThan(Integer target) {
-        checkIfActualNull();
-        if (target == null) {
-            throw new NullPointerException("b can not be null in a > b");
-        }
-        if (actual > target) {
-            reportSuccess();
-            return self();
-        }
-        return fail("Expected %s to be greater than %s ", actual, target);
+        return test(actual -> actual > target,"Expected %s to be greater than %s ", actual, target);
     }
 
     public IntegerAssertion isGreaterThanOrEqual(Integer target) {
-        checkIfActualNull();
-        if (target == null) {
-            throw new NullPointerException("b can not be null in a >= b");
-        }
-        if (actual >= target) {
-            reportSuccess();
-            return self();
-        }
-        return fail("Expected %s to be greater than or equal to %s ", actual, target);
+       return test(actual -> actual >= target,"Expected %s to be greater than or equal to %s ", actual, target);
     }
 
     public IntegerAssertion isBetween(Integer lower, Integer upper) {
-        checkIfActualNull();
-        if (lower == null) {
+         if (lower == null) {
             throw new NullPointerException("Lower bound can not be null");
         }
         if (upper == null) {
             throw new NullPointerException("Upper bound can not be null");
         }
-        if (actual >= lower && actual <= upper) {
-            reportSuccess();
-            return self();
-        }
-        return fail("Expected %s to be between %s ", actual, lower, upper);
+        return test(actual -> actual >= lower && actual <= upper,"Expected %s to be between %s ", actual, lower, upper);
     }
 
     public IntegerAssertion isMultipleOf(Integer multiplicant) {
-        checkIfActualNull();
         if (multiplicant == null) {
             throw new NullPointerException("multiplicant can not be null");
         }
-        if (actual % multiplicant == 0) {
-            reportSuccess();
-            return self();
-        }
-        return fail("Expected %s to be a multiple of %s ", actual, multiplicant);
+        return test(actual -> actual % multiplicant == 0,"Expected %s to be a multiple of %s ", actual, multiplicant);
     }
 }
