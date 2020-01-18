e177c95026913c73f39b2f9b854d8e4fd4ed9171
igoudt <igoudt@bol.com>
2020-01-18 11:45:57 +0100

    add throwIfNull to BigInteger

--- src/main/java/io/github/ingmargoudt/veritas/assertions/BigIntegerAssertion.java
+++ src/main/java/io/github/ingmargoudt/veritas/assertions/BigIntegerAssertion.java
@@ -11,29 +11,32 @@ public class BigIntegerAssertion extends AbstractAssertion<BigIntegerAssertion, 
 
     @Override
     public BigIntegerAssertion isSmallerThan(BigInteger target) {
+        throwIfNull(target, "in a < b , b can not be null");
         return test(actual -> actual.compareTo(target) < 0, "%s was not smaller than %s", actual, target);
     }
 
     @Override
     public BigIntegerAssertion isSmallerThanOrEqual(BigInteger target) {
+        throwIfNull(target, "in a <= b , b can not be null");
         return test(actual -> actual.compareTo(target) <= 0, "%s was not smaller than or equal to %s", actual, target);
     }
 
     @Override
     public BigIntegerAssertion isGreaterThan(BigInteger target) {
-
+        throwIfNull(target, "in a > b , b can not be null");
         return test(actual -> actual.compareTo(target) > 0, "%s was not greater than %s", actual, target);
     }
 
     @Override
     public BigIntegerAssertion isGreaterThanOrEqual(BigInteger target) {
-
+        throwIfNull(target, "in a >= b , b can not be null");
         return test(actual -> actual.compareTo(target) >= 0, "%s was not greater than or equal to %s", actual, target);
     }
 
     @Override
     public BigIntegerAssertion isBetween(BigInteger lower, BigInteger upper) {
-
+        throwIfNull(lower, "Lower bound can not be null");
+        throwIfNull(upper, "Upper bound can not be null");
         return test(actual -> actual.compareTo(lower) >= 0 && actual.compareTo(upper) <= 0, "%s is not between %s and %s", actual, lower, upper);
     }
 }
