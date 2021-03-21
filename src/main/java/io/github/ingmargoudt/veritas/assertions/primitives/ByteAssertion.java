bc8490579ea306bd30ffe9a261359ef080669bce
igoudt <igoudt@bol.com>
2021-03-21 08:49:26 +0100

    implement Byte Assertion

--- src/main/java/io/github/ingmargoudt/veritas/assertions/primitives/ByteAssertion.java
+++ src/main/java/io/github/ingmargoudt/veritas/assertions/primitives/ByteAssertion.java
@@ -6,4 +6,26 @@ public class ByteAssertion extends AbstractAssertion<ByteAssertion, Byte> {
     public ByteAssertion(Byte actual) {
         super(actual);
     }
+
+
+    public ByteAssertion hasValue(int value){
+        return test(actual -> actual.intValue() == value,"Byte %s has not int value of %s", actual, value);
+
+    }
+
+    public ByteAssertion hasValue(double value) {
+       return test(actual -> actual.doubleValue() == value, "Byte %s has not double value of %s", actual, value);
+    }
+
+    public ByteAssertion hasValue(float value){
+        return test(actual -> actual.floatValue() == value, "Byte %s has not float value of %s",actual, value);
+    }
+
+    public ByteAssertion hasValue(long value){
+        return test(actual -> actual.longValue() == value, "Byte %s has not long value of %s", actual, value);
+    }
+
+    public ByteAssertion hasValue(short value){
+        return test(actual -> actual.shortValue() == value, "Byte %s has not short value of %s", actual, value);
+    }
 }
--- src/test/java/ByteAssertionTest.java
+++ src/test/java/ByteAssertionTest.java
@@ -10,4 +10,36 @@ public class ByteAssertionTest {
         Byte c = new Byte("10");
         assertThat(b).isEqualTo(c);
     }
+
+    @Test
+    public void hasIntValue(){
+        Byte b  = new Byte("10");
+        assertThat(b).hasValue(10);
+    }
+
+    @Test
+    public void hasDoubleValue(){
+        Byte b = new Byte("10");
+        assertThat(b).hasValue(10.0);
+    }
+
+
+    @Test
+    public void hasFloatValue(){
+        Byte b = new Byte("10");
+        assertThat(b).hasValue(10.0f);
+    }
+
+    @Test
+    public void hasShortValue(){
+        Byte b = new Byte("10");
+        short v = 10;
+        assertThat(b).hasValue(v);
+    }
+
+    @Test
+    public void hasLongValue(){
+        Byte b = new Byte("10");
+        assertThat(b).hasValue(10L);
+    }
 }
